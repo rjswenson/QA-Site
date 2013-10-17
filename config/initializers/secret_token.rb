e41a62ebec9f0fd10887338ae5f2d90b3409475a
Robin Swenson-Healey <rjswenson@gmail.com>
2013-10-17 13:32:00 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QASite::Application.config.secret_token = 'a9be5d7c248cc6d29ea4bfb9a48a35ea55d6ba91d2415931953042bfde086f3a10bd4ce1185328e8e3ebfbc7cbd210b4d021c4aec48c6af31b8c7b1cc13fc7fa'
